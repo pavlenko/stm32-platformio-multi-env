00ded7d78d6092de70fdd4a866e24e3bf7bc43ad
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-31 12:32:33 +0300

    upd

--- env/bootloader_dfu/src/main.c
+++ env/bootloader_dfu/src/main.c
@@ -7,7 +7,7 @@
 
 #include "config.h"
 
-#define APP_BASE (FLASH_BASE + DFU_SIZE)
+#define APP_ADDRESS (FLASH_BASE + DFU_SIZE)
 
 // Enables DFU mode when a reset from the nRST pin occurs.
 static inline uint32_t reset_due_to_pin(void) {
@@ -20,27 +20,38 @@ static inline uint32_t reset_due_to_watchdog(void) {
     return (RCC_CSR & RCC_CSR_IWDGRSTF);
 }
 
-// If some pin has some state
-static int force_dfu_gpio(void) {
-	rcc_gpio_enable(GPIO_DFU_BOOT_PORT);
-	gpio_set_input_pp(GPIO_DFU_BOOT_PORT, GPIO_DFU_BOOT_PIN);
-	gpio_clear(GPIO_DFU_BOOT_PORT, GPIO_DFU_BOOT_PIN);
-	for (unsigned int i = 0; i < 512; i++)
-		__asm__("nop");
-	uint16_t val = gpio_read(GPIO_DFU_BOOT_PORT, GPIO_DFU_BOOT_PIN);
-	gpio_set_input(GPIO_DFU_BOOT_PORT, GPIO_DFU_BOOT_PIN);
-	return val != 0;
-}
-
 int main(void) {
-    rcc_clock_setup_in_hse_8mhz_out_72mhz();
-
     //TODO maybe check RTC backup registers instead of ram (do not need ram remap)
     //TODO or check if: last 8 RAM bytes = DFU_KEY - got to dfu
     //TODO or check if: reset_due_to_pin
     //TODO or check if: reset_due_to_watchdog
-    //TODO or check if: force_dfu_gpio
     //TODO or check if: invalid checksum
+
+#ifndef DFU_CHECK_GPIO_DISABLED
+    // check force_dfu_gpio
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_set_mode(DFU_CHECK_GPIO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, DFU_CHECK_GPIO_PIN);
+    gpio_clear(DFU_CHECK_GPIO_PORT, DFU_CHECK_GPIO_PIN);
+    for (uint16_t i = 0; i < 512; i++) {
+        __asm__("nop");
+    }
+    bool go_dfu = gpio_get(DFU_CHECK_GPIO_PORT, DFU_CHECK_GPIO_PIN);
+    gpio_set_mode(DFU_CHECK_GPIO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, DFU_CHECK_GPIO_PIN);
+#endif
+
+    if (!go_dfu) {
+        // Set vector table base address.
+        volatile uint32_t *_csb_vtor = (uint32_t*)0xE000ED08U;
+        *_csb_vtor = APP_ADDRESS & 0xFFFF;
+
+        // Initialise master stack pointer.
+        __asm__ volatile("msr msp, %0"::"g" (*(volatile uint32_t *)APP_ADDRESS));
+
+        // Jump to application.
+        (*(void (**)(void))(APP_ADDRESS + 4))();
+    }
+
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
     while (1) {
         
