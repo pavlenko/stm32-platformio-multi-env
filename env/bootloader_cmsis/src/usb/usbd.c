1546991eeedc9521e94b4e9180aab9e8482db200
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-06-03 08:08:12 +0300

    upd

--- env/bootloader_cmsis/src/usb/usbd.c
+++ env/bootloader_cmsis/src/usb/usbd.c
@@ -13,8 +13,51 @@ void usb_irq_handler(void) {
     }
 
     if (USB->ISTR & USB_ISTR_RESET) {
-        USB->ISTR &= ~USB_ISTR_RESET;
+        USB->ISTR &= (uint16_t) ~USB_ISTR_RESET;
         //TODO (void)HAL_PCD_SetAddress(hpcd, 0U);
+    }
+
+    if (USB->ISTR & USB_ISTR_PMAOVR) {
+        USB->ISTR &= (uint16_t) ~USB_ISTR_PMAOVR;
+    }
+
+    if (USB->ISTR & USB_ISTR_ERR) {
+        USB->ISTR &= (uint16_t) ~USB_ISTR_ERR;
+    }
+
+    if (USB->ISTR & USB_ISTR_WKUP) {
+        USB->CNTR &= (uint16_t) ~USB_CNTR_LP_MODE;
+        USB->CNTR &= (uint16_t) ~USB_CNTR_FSUSP;
+
+        //TODO HAL_PCD_ResumeCallback(hpcd);
+
+        USB->ISTR &= (uint16_t) ~USB_ISTR_WKUP;
+    }
+
+    if (USB->ISTR & USB_ISTR_SUSP) {
+        /* WA: To Clear Wakeup flag if raised with suspend signal */
+
+        uint8_t i, store_ep[8];
+        for (i = 0U; i < 8U; i++) {
+            store_ep[i] = PCD_GET_ENDPOINT(hpcd->Instance, i);/* Store Endpoint register */
+        }
+
+        USB->CNTR |= (uint16_t) USB_CNTR_FRES;/* FORCE RESET */
+        USB->CNTR &= (uint16_t) ~USB_CNTR_FRES;/* CLEAR RESET */
+
+        while ((USB->ISTR & USB_ISTR_RESET) == 0U);/* wait for reset flag in ISTR */
+
+        USB->ISTR &= (uint16_t) USB_ISTR_RESET;/* Clear Reset Flag */
+
+        for (i = 0U; i < 8U; i++) {
+            PCD_SET_ENDPOINT(hpcd->Instance, i, store_ep[i]);/* Restore Endpoint register */
+        }
+
+        USB->CNTR |= (uint16_t) USB_CNTR_FSUSP;/* Force low-power mode in the macro cell */
+        USB->ISTR &= (uint16_t) ~USB_ISTR_SUSP;/* clear of the ISTR bit must be done after setting of CNTR_FSUSP */
+        USB->CNTR |= (uint16_t) USB_CNTR_LP_MODE;
+
+        //TODO HAL_PCD_SuspendCallback(hpcd);
     }
 
 #if (IMR_MSK & ISTR_SOF)
@@ -45,39 +88,6 @@ void usb_irq_handler(void) {
     _SetISTR((uint16_t)CLR_ERR);
 #ifdef ERR_CALLBACK
     ERR_Callback();
-#endif
-  }
-#endif
-    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_WKUP)
-    if (wIstr & ISTR_WKUP & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_WKUP);
-    Resume(RESUME_EXTERNAL);
-#ifdef WKUP_CALLBACK
-    WKUP_Callback();
-#endif
-  }
-#endif
-    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_SUSP)
-    if (wIstr & ISTR_SUSP & wInterrupt_Mask)
-  {
-
-    /* check if SUSPEND is possible */
-    if (fSuspendEnabled)
-    {
-      Suspend();
-    }
-    else
-    {
-      /* if not possible then resume after xx ms */
-      Resume(RESUME_LATER);
-    }
-    /* clear of the ISTR bit must be done after setting of CNTR_FSUSP */
-    _SetISTR((uint16_t)CLR_SUSP);
-#ifdef SUSP_CALLBACK
-    SUSP_Callback();
 #endif
   }
 #endif
