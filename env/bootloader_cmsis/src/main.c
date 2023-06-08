657c125f7eae41c3e7eddd208cf918d387890057
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-06-08 20:11:24 +0300

    upd

--- env/bootloader_cmsis/src/main.c
+++ env/bootloader_cmsis/src/main.c
@@ -1,6 +1,53 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/*
+ * USB Device
+ * - device descriptor (num configurations, vid/pid)
+ *   - configuration descriptor (power, num interfaces, index of string descriptor)
+ *     - interface descriptor (class, subclass, protocol)
+ *       - endpoint descriptor (setup/in/out, index of string descriptor)
+ */
+
+#define USB_DESCRIPTOR_TYPE_DEVICE    0x01
+#define USB_DESCRIPTOR_TYPE_CONFIG    0x02
+#define USB_DESCRIPTOR_TYPE_STRING    0x03
+#define USB_DESCRIPTOR_TYPE_INTERFACE 0x04
+#define USB_DESCRIPTOR_TYPE_ENDPOINT  0x05
+
+const uint8_t usb_device_descriptor[] = {
+    0x12, // bLength: Device descriptor length = 0x12 - default
+    0x01, // bDescriptorType: Descriptor Type = USB_DESCRIPTOR_TYPE_DEVICE - default
+    0x00, // bcdUSB: USB Specification Release Number = (L byte of 0x0200)
+    0x02, // bcdUSB: USB Specification Release Number = (H byte of 0x0200)
+    0x00, // bDeviceClass: Class Code = 0x00 - for define in interfaces
+    0x00, // bDeviceSubClass: Subclass code = 0x00 - for define in interfaces
+    0x00, // bDeviceProtocol: Protocol code = 0x00 - for
+    0x40, // bMaxPacketSize40: Maximum packet size for endpoint zero = 0x40 = 64
+    0x83, // idVendor: Vendor ID = (L byte of 0x0483)
+    0x04, // idVendor: Vendor ID = (H byte of 0x0483)
+    0x50, // idProduct: Product ID = (L byte of 0x5750)
+    0x57, // idProduct: Product ID = (H byte of 0x5750)
+    0x00, // bcdDevice: Device release number = (L byte of 0x0200)
+    0x02, // bcdDevice: Device release number = (H byte of 0x0200)
+    0x01, // iManufacturer: Index of string descriptor describing manufacturer
+    0x02, // iProduct: Index of string descriptor describing product
+    0x03, // iSerialNumber: Index of string descriptor describing the device serial number
+    0x01, // bNumConfigurations: Number of possible configurations ()
+};
+
+const uint8_t usb_config_descriptor[] = {
+    0x09, // bLength: Configuration Descriptor size = 0x09 - default
+    0x02, // bDescriptorType: Descriptor Type = USB_DESCRIPTOR_TYPE_CONFIG - default
+    0x00, // wTotalLength: Bytes returned = (L byte) - depends on configuration sub-descriptors
+    0x00, // wTotalLength: Bytes returned = (H byte)
+    0x02, // bNumInterfaces: Number of interfaces = 2 depends on configuration, > 1 for composite device
+    0x01, // bConfigurationValue: Configuration value for use in SetConfiguration()
+    0x00, // iConfiguration: Index of string descriptor describing the configuration
+    0xC0, // bmAttributes: Configuration characteristics mask = 0xC0 (self-powered) - see specification
+    0x32, // bMaxPower: Maximum power consumption = 100 mA - this current is used for detecting VBus
+};
+
 int main(void) {
     while (1) {
     }
